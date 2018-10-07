675f2aa7550963b28cd32b5ed73adc0a376e1b98
agungdwiprasetyo <agungdwiprasetyo22@gmail.com>
2018-10-07 08:43:37 +0700

    update fontawesome url

--- pages/components/head.js
+++ pages/components/head.js
@@ -12,7 +12,7 @@ class HeadComponent extends PureComponent {
                     SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
                 <link href="https://agungdp.storage.googleapis.com/wedding/static/css/bootstrap.css" type="text/css" rel="stylesheet" media="all" />
                 <link href="https://agungdp.storage.googleapis.com/wedding/static/css/style.css" type="text/css" rel="stylesheet" media="all" />  
-                {/* <link href="https://agungdp.storage.googleapis.com/wedding/static/css/font-awesome.css" rel="stylesheet" />  */}
+                <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" /> 
                 <link rel="stylesheet" href="https://agungdp.storage.googleapis.com/wedding/static/css/swipebox.css" /> 
                 <link href="//fonts.googleapis.com/css?family=Tulpen+One" rel="stylesheet" />
                 <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet" />
