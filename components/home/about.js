266b1d44f6de5eeabc632b3152e0180d1b57ec49
agungdwiprasetyo <agungdwiprasetyo22@gmail.com>
2018-10-23 23:58:29 +0700

    change event to dynamic from api

--- components/home/about.js
+++ components/home/about.js
@@ -1,11 +1,52 @@
 import React, {PureComponent} from "react";
+import API from "../../pages/helper/helper";
 
 export default class AboutComponent extends PureComponent {
     constructor(props) {
         super(props);
+
+        this.state = {
+            loading: true,
+            event: {}
+        };
+
+        this.api = new API;
+
+        this.getEvents = this.getEvents.bind(this);
+
+        this.getEvents();
+    }
+    
+    getEvents() {
+        this.setState({
+            loading: true
+        });
+        this.api.apiGet("invitation/event").then(response => {
+            this.setState({loading: false});
+            this.setState({event: response.data});
+        }).catch(err => {
+            console.log(err);
+            this.setState({
+                loading: false,
+                event: {}
+            });
+        })
     }
     
     render() {
+        const {
+            loading,
+            event
+        } = this.state;
+
+        const Event = (props)=>{
+            return (
+                <main>
+                    {props.name}: <b>{props.data}</b>
+                </main>
+            );
+        }
+
         return(
             <div id="about" className="about w3-agile">
                 <div className="container">
@@ -32,15 +73,71 @@ export default class AboutComponent extends PureComponent {
                     <div className="contact-info">	
                         <div className="col-md-12 text-center">
                             <div className="cnt-address">
-                                <h5>Sunday, December 30, 2018</h5> 
-                                <p className="akad">Ceremony: <b>09.00 a.m</b>
-                                    <span>Reception: <b>01.00 p.m - finish</b></span>
-                                    Jl. H. Liyas II No.77, Paninggilan Utara, Ciledug, Kota Tangerang, Banten 15153
+                                <h5> { loading ? <div className="animated-background date-loading"></div> : event.date } </h5> 
+                                <p className="akad"> { loading ? <div className="animated-background ceremony-loading"></div> : <Event name="Ceremony" data={event.ceremony} /> }
+                                    <span>{ loading ? <div className="animated-background reception-loading"></div> : <Event name="Reception" data={event.reception} /> }</span>
+                                    { loading ? <div className="animated-background address-loading"></div> : event.address }
                                 </p> 
                             </div>
                         </div>
                     </div>
                 </div>
+
+
+                <style jsx>{`
+                    .img-people {
+                        display: block;
+                        height: auto;
+                        max-width: 100%;
+                        border: 0;
+                        margin-left: auto;
+                        margin-right: auto;
+                    }
+                    
+                    @keyframes placeHolderShimmer{
+                        0%{
+                            background-position: -468px 0
+                        }
+                        100%{
+                            background-position: 468px 0
+                        }
+                    }
+                    
+                    .animated-background {
+                        background: #fff;
+                        margin: 0 auto;
+
+                        animation-duration: 1s;
+                        animation-fill-mode: forwards;
+                        animation-iteration-count: infinite;
+                        animation-name: placeHolderShimmer;
+                        animation-timing-function: linear;
+                        background: #f6f7f8;
+                        background: linear-gradient(to right, #eeeeee 8%, #dddddd 18%, #eeeeee 33%);
+                        background-size: 800px 104px;
+                        height: 10px;
+                        position: relative;
+                    }
+
+                    .date-loading {
+                        max-width: 372px;
+                        min-height: 40px;
+                    }
+                    .ceremony-loading {
+                        max-width: 172px;
+                        min-height: 30px;
+                    }
+                    .reception-loading {
+                        margin-top: 5px;
+                        max-width: 250px;
+                        min-height: 30px;
+                    }
+                    .address-loading {
+                        margin-top: 20px;
+                        max-width: 672px;
+                        min-height: 30px;
+                    }
+                `}</style>
             </div> 
         );
     }
