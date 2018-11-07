d1ecdb5772bccd0d3479681594a66ac8529cb04d
agungdwiprasetyo <agungdwiprasetyo22@gmail.com>
2018-11-07 23:36:48 +0700

    change base font style

--- components/Layout.js
+++ components/Layout.js
@@ -20,11 +20,9 @@ export default class Layout extends PureComponent {
                 <link href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.28.4/sweetalert2.min.css" type="text/css" rel="stylesheet" media="all" />  
                 <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" /> 
                 <link rel="stylesheet" href="https://agungdp.storage.googleapis.com/wedding/static/css/swipebox.css" /> 
-                <link href="//fonts.googleapis.com/css?family=Tulpen+One" rel="stylesheet" />
-                <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet" />
                 <link href="https://fonts.googleapis.com/css?family=KoHo:300" rel="stylesheet" />
                 <link href="https://fonts.googleapis.com/css?family=Euphoria+Script&amp;subset=latin-ext" rel="stylesheet" />
-                <link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css' />
+                <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
                 <link rel="shortcut icon" href="https://agungdp.storage.googleapis.com/wedding/static/iconheader.png" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
